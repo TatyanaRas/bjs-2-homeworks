06741490af4d08e8fc1cb7ad184f288613c89a86
Tatyana Rasskazova <tatyanalipch@yandex.ru>
2024-09-05 18:38:44 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 -4 * a * c;
+	if (d > 0) {
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+		
+	} else if (d === 0) {
+		arr[0] = -b / (2 * a);
+		
+	} else {
+		arr = [];
+			}
+	return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+//function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
-}
+//}
