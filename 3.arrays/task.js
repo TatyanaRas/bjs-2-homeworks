2e1b43f47bdf057fb02d662e6ce10b7ae8618a5c
Tatyana Rasskazova <tatyanalipch@yandex.ru>
2024-09-13 12:42:36 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
+
+//Задача 1 сравнить массивы
 function compareArrays(arr1, arr2) {
-  
+   
+  return  arr1.length === arr2.length && arr1.every((values, index) => values === arr2[index]);
+   } 
+   
+//Задача 2 фильтрация и преобразование массива
+function getUsersNamesInAgeRange(users, gender) {
+
+    let result = users.filter(values => values.gender === gender).map(values => values.age).reduce((acc, values, index, arr)=> 
+    {acc+=values;
+        if (index === arr.length - 1) {
+            return acc / arr.length;
+        }
+return acc;
+}, 0);
+return result;
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
