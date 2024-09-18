a72f98cd668c462e6fc1253bec987c5de04fc60e
Tatyana Rasskazova <tatyanalipch@yandex.ru>
2024-09-18 09:51:08 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,45 +1,40 @@
 function Student(name, gender, age) {
- this.name = name;
- this.gender = gender;
- this.age = age;
-this.marks = [];
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 
-/*let student1 = new Student("Василиса", "женский", 19);
-let student2 = new Student("Артём", "мужской", 25);*/
 }
 
-Student.prototype.setSubject = function (subjectName) {
-this.subject = subjectName;
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+
 }
 
 
-Student.prototype.addMarks = function (...marksToAdd) {
-  if (!this.marks) {
-  // (this.marks === undefined) {
-    return;
-    //this.marks = [...marksToAdd]
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.marks) {
+		return;
+
     }
+		if (!!marksToAdd.length) {
+			this.marks.push(...marksToAdd)
+		}
 
-   if (!!marksToAdd.length) {
-    this.marks.push(...marksToAdd)
-}
-  
-   return;
-}
+		return;
+	}
 
-Student.prototype.getAverage = function () {
-    if (!this.marks || !this.marks.length) {
-        return 0;
-        }
-        else {
-       return  this.marks.reduce((acc, curr) => acc + curr / this.marks.length, 0);
-        }
-}
+	Student.prototype.getAverage = function() {
+		if (!this.marks || !this.marks.length) {
+			return 0;
+		} else {
+			return this.marks.reduce((acc, curr) => acc + curr / this.marks.length, 0);
+		}
+	}
 
-Student.prototype.exclude = function (reason) {
-delete this.subjectName;
-    delete this.marks;
-    this.excluded = reason;
-  
-}
+	Student.prototype.exclude = function(reason) {
+		delete this.subjectName;
+		delete this.marks;
+		this.excluded = reason;
+
+	}
